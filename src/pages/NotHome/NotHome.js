56c000bda58f06d4c62008221eed5c88043e798c
alexbawa <alexbawa@gmail.com>
2022-11-06 12:12:41 -0600

    Set up file structure, example w/ router

---
+++ src/pages/NotHome/NotHome.js
@@ -0,0 +1,11 @@
+import ExampleButtonComponent from "../../components/ExampleButtonComponent/ExampleButtonComponent";
+
+function NotHome() {
+    return (
+        <div>
+            <ExampleButtonComponent pageName="NotHome"/>
+        </div>
+    );
+};
+
+export default NotHome;

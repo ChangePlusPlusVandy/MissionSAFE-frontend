175551acaaa692d2efcda44e344c30853bdea795
alexbawa <alexbawa@gmail.com>
2022-12-09 13:12:20 -0600

    Wipe examples, start scaffolding

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
 
-import Home from "./pages/Home/Home";
-import NotHome from "./pages/NotHome/NotHome";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/not-home" element={<NotHome/>}/>
-      </Routes>
-    </BrowserRouter>
-  );
+    this.state = {
+      user: null,
+    };
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>Hello from localhost:3000!</div>}/>
+          <Route path="/next" element={<div>Hello from localhost:3000/next!</div>}/>
+          <Route path="/last" element={<div>Hello from localhost:3000/last!</div>}/>
+        </Routes>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
--- src/components/ExampleButtonComponent/ExampleButtonComponent.js
+++
@@ -1,5 +0,0 @@
-function ExampleButtonComponent(props) {
-    return <p>Hello, you are on {props.pageName}!</p>
-}
-
-export default ExampleButtonComponent;

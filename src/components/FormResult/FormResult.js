6f8b3bb792d02607e8769a41f59aa9afbfdf915a
alexbawa <alexbawa@gmail.com>
2023-01-31 15:51:50 +0100

    form tweak

--- src/components/FormResult/FormResult.js
+++ src/components/FormResult/FormResult.js
@@ -7,7 +7,7 @@ class FormResult extends React.Component {
       <div className="form-result">
         <div className="form-result-left">
           <p className="form-title">Form filed {this.props.form.date}</p>
-          <p className="form-programs">Regarding {this.props.programs.join(", ")}</p>
+          <p className="form-programs">Regarding {this.props.form.programs.join(", ")}</p>
         </div>
       </div>
     );

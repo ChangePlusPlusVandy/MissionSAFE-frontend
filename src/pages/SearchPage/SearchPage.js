39aaaec0ebcea44c5b5415ca11164058a689ed85
Jianger <ethantianshujiang@gmail.com>
2023-03-27 04:48:34 +0200

    added export modal, needs to be linked to actual PDF generation function:

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Modal from "react-modal";
 import SearchResults from "../../components/SearchResults/SearchResults";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import logo from './assets/mission-safe-logo1.png'
@@ -10,57 +11,70 @@ class SearchPage extends React.Component {
         formResults: [],
         eventResults: [],
         date: dateFormat(new Date(), "fullDate"),
-        searchSummary: ""
-        };
+        searchSummary: "",
+        showExportModal: false
+    };
 
-    updateEventResults = (criteria, text) =>{
+    toggleExportModal = () => {
+        this.setState({
+            showExportModal: !this.state.showExportModal
+        })
+    }
+
+    exportResultsToPDF = () => {
+        console.log("PDF export button clicked")
+        // TODO: call the actual PDF generation code here
+        // createPDF(this.state.youthResults, this.state.formResults, this.state.eventResults)
+    }
+
+    updateEventResults = (criteria, text) => {
         //call backend for real results later
         var summary = "Showing results";
-        if (text.length>0){
-            summary+=" for \"" + text + "\""
+        if (text.length > 0) {
+            summary += " for \"" + text + "\""
         }
-        if (criteria.length>0){
-            summary+=" by " + criteria
+        if (criteria.length > 0) {
+            summary += " by " + criteria
         }
-        summary+=" within Events"
+        summary += " within Events"
         //dummy results
         var results = [{
-            name: "Event 1" ,
-            programs: ["1","2","3"],
+            name: "Event 1",
+            programs: ["1", "2", "3"],
             date: "01/01/2000",
             attended_youth: ["Youth1", "Youth2", "Youth3"],
-            attached_forms: ["Form1", "Form2", "form3"], 
+            attached_forms: ["Form1", "Form2", "form3"],
             _id: 1
-            },{
-                name: "Event 2" ,
-                programs: ["1","2","3"],
-                date: "02/01/2000",
-                attended_youth: ["Youth1", "Youth2", "Youth3"],
-                attached_forms: ["Form1", "Form2", "form3"], 
-                _id: 2
-                }]
+        }, {
+            name: "Event 2",
+            programs: ["1", "2", "3"],
+            date: "02/01/2000",
+            attended_youth: ["Youth1", "Youth2", "Youth3"],
+            attached_forms: ["Form1", "Form2", "form3"],
+            _id: 2
+        }]
         //set State
         this.setState({
             youthResults: [],
             formResults: [],
             eventResults: results,
             searchSummary: summary
-            });
+        });
     }
     updateYouthResults = (criteria, text) => {
         //Call to backend for results later
         var summary = "Showing results";
-        if (text.length>0){
-            summary+=" for \"" + text + "\""
+        if (text.length > 0) {
+            summary += " for \"" + text + "\""
         }
-        if (criteria.length>0){
-            summary+=" by " + criteria
+        if (criteria.length > 0) {
+            summary += " by " + criteria
         }
-        summary+=" within Youth"
+        summary += " within Youth"
         //Dummy Results
-        var results = [{fireID: 1, firstName: "first", lastName: "Test", email: "test1.com", programs: ["1","2","3"], active: true},
-        {fireID: 2, firstName: "second", lastName: "Test", email: "test2.com", programs: ["1","2","3"], active: true}];
-             //add results to youthResults
+        var results = [{ fireID: 1, firstName: "first", lastName: "Test", email: "test1.com", programs: ["1", "2", "3"], active: true },
+        { fireID: 2, firstName: "second", lastName: "Test", email: "test2.com", programs: ["1", "2", "3"], active: true }];
+        //add results to youthResults
         this.setState({
             youthResults: results,
             formResults: [],
@@ -68,19 +82,19 @@ class SearchPage extends React.Component {
             searchSummary: summary
         });
     }
-    updateFormResults = (criteria, text) =>{
+    updateFormResults = (criteria, text) => {
         //Call to backend for results later
         var summary = "Showing results";
-        if (text.length>0){
-            summary+=" for \"" + text + "\""
+        if (text.length > 0) {
+            summary += " for \"" + text + "\""
         }
-        if (criteria.length>0){
-            summary+=" by " + criteria
+        if (criteria.length > 0) {
+            summary += " by " + criteria
         }
-        summary+=" within Forms"
+        summary += " within Forms"
         //Dummy Results
-        var results = [{name: "Form 1" , _id: 1 , date: "01/01/2000", programs: ["1","2","3"], staff: "John"},
-        {name: "Form 2" , _id: 2 , date: "02/01/2000", programs: ["1","2","3"], staff: "John"}]
+        var results = [{ name: "Form 1", _id: 1, date: "01/01/2000", programs: ["1", "2", "3"], staff: "John" },
+        { name: "Form 2", _id: 2, date: "02/01/2000", programs: ["1", "2", "3"], staff: "John" }]
         this.setState({
             youthResults: [],
             formResults: results,
@@ -92,20 +106,28 @@ class SearchPage extends React.Component {
         return (
             <div className="column-container">
                 <div className="logo-container">
-                    <img className = "logo" src = {logo} alt= ""/>
+                    <img className="logo" src={logo} alt="" />
                 </div>
-                <div className = 'bar-container'>
+                <div className='bar-container'>
                     <h1 className="date">{this.state.date}</h1>
-                    <SearchBar updateYouthResults = {this.updateYouthResults} updateFormResults = {this.updateFormResults}
-                    updateEventResults = {this.updateEventResults}/>
-                </div>
+                    <SearchBar updateYouthResults={this.updateYouthResults} updateFormResults={this.updateFormResults}
+                        updateEventResults={this.updateEventResults} />
+                    <button className="export-button" onClick={this.toggleExportModal}>Export Results to PDF</button>
+                    <Modal isOpen={this.state.showExportModal} onRequestClose={this.toggleExportModal} appElement={document.getElementById('root')} className="export-modal">
+                        <div className="column-container modal-container">
+                            <h2 className="export-modal-header">Export Options</h2>
+                            <p>Put export options here.</p>
+                            <button className="export-button" onClick={this.exportResultsToPDF}>Export to PDF</button>
+                        </div>
+                    </Modal>
+                </div >
                 <div className="search-summary">
-                <p>{this.state.searchSummary}</p>
+                    <p>{this.state.searchSummary}</p>
                 </div>
-                <div className = 'results-container'>
-                    <SearchResults youthResults = {this.state.youthResults}  formResults = {this.state.formResults} eventResults = {this.state.eventResults}/>
+                <div className='results-container'>
+                    <SearchResults youthResults={this.state.youthResults} formResults={this.state.formResults} eventResults={this.state.eventResults} />
                 </div>
-            </div>
+            </div >
         )
     }
 }

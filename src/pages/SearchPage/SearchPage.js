a8123db5d2180873a22f0dfa4e0149d6b38b55fe
Jhanavi Thakkar <jhanavi.a.thakkar@vanderbilt.edu>
2023-04-07 18:20:34 -0500

    added pdf generator component and added that button to search page

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -4,6 +4,9 @@ import SearchBar from "../../components/SearchBar/SearchBar";
 import logo from './assets/mission-safe-logo1.png'
 import './SearchPage.scss'
 import dateFormat from "dateformat";
+import ReportGenerator from "../../components/ReportGenerator";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+
 class SearchPage extends React.Component {
     state = {
         youthResults: [],
@@ -11,56 +14,56 @@ class SearchPage extends React.Component {
         eventResults: [],
         date: dateFormat(new Date(), "fullDate"),
         searchSummary: ""
-        };
+    };
 
-    updateEventResults = (criteria, text) =>{
+    updateEventResults = (criteria, text) => {
         //call backend for real results later
         var summary = "Showing results";
-        if (text.length>0){
-            summary+=" for \"" + text + "\""
+        if (text.length > 0) {
+            summary += " for \"" + text + "\""
         }
-        if (criteria.length>0){
-            summary+=" by " + criteria
+        if (criteria.length > 0) {
+            summary += " by " + criteria
         }
-        summary+=" within Events"
+        summary += " within Events"
         //dummy results
         var results = [{
-            name: "Event 1" ,
-            programs: ["1","2","3"],
+            name: "Event 1",
+            programs: ["1", "2", "3"],
             date: "01/01/2000",
             attended_youth: ["Youth1", "Youth2", "Youth3"],
-            attached_forms: ["Form1", "Form2", "form3"], 
+            attached_forms: ["Form1", "Form2", "form3"],
             _id: 1
-            },{
-                name: "Event 2" ,
-                programs: ["1","2","3"],
-                date: "02/01/2000",
-                attended_youth: ["Youth1", "Youth2", "Youth3"],
-                attached_forms: ["Form1", "Form2", "form3"], 
-                _id: 2
-                }]
+        }, {
+            name: "Event 2",
+            programs: ["1", "2", "3"],
+            date: "02/01/2000",
+            attended_youth: ["Youth1", "Youth2", "Youth3"],
+            attached_forms: ["Form1", "Form2", "form3"],
+            _id: 2
+        }]
         //set State
         this.setState({
             youthResults: [],
             formResults: [],
             eventResults: results,
             searchSummary: summary
-            });
+        });
     }
     updateYouthResults = (criteria, text) => {
         //Call to backend for results later
         var summary = "Showing results";
-        if (text.length>0){
-            summary+=" for \"" + text + "\""
+        if (text.length > 0) {
+            summary += " for \"" + text + "\""
         }
-        if (criteria.length>0){
-            summary+=" by " + criteria
+        if (criteria.length > 0) {
+            summary += " by " + criteria
         }
-        summary+=" within Youth"
+        summary += " within Youth"
         //Dummy Results
-        var results = [{fireID: 1, firstName: "first", lastName: "Test", email: "test1.com", programs: ["1","2","3"], active: true},
-        {fireID: 2, firstName: "second", lastName: "Test", email: "test2.com", programs: ["1","2","3"], active: true}];
-             //add results to youthResults
+        var results = [{ fireID: 1, firstName: "first", lastName: "Test", email: "test1.com", programs: ["1", "2", "3"], active: true },
+        { fireID: 2, firstName: "second", lastName: "Test", email: "test2.com", programs: ["1", "2", "3"], active: true }];
+        //add results to youthResults
         this.setState({
             youthResults: results,
             formResults: [],
@@ -68,19 +71,19 @@ class SearchPage extends React.Component {
             searchSummary: summary
         });
     }
-    updateFormResults = (criteria, text) =>{
+    updateFormResults = (criteria, text) => {
         //Call to backend for results later
         var summary = "Showing results";
-        if (text.length>0){
-            summary+=" for \"" + text + "\""
+        if (text.length > 0) {
+            summary += " for \"" + text + "\""
         }
-        if (criteria.length>0){
-            summary+=" by " + criteria
+        if (criteria.length > 0) {
+            summary += " by " + criteria
         }
-        summary+=" within Forms"
+        summary += " within Forms"
         //Dummy Results
-        var results = [{name: "Form 1" , _id: 1 , date: "01/01/2000", programs: ["1","2","3"], staff: "John"},
-        {name: "Form 2" , _id: 2 , date: "02/01/2000", programs: ["1","2","3"], staff: "John"}]
+        var results = [{ name: "Form 1", _id: 1, date: "01/01/2000", programs: ["1", "2", "3"], staff: "John" },
+        { name: "Form 2", _id: 2, date: "02/01/2000", programs: ["1", "2", "3"], staff: "John" }]
         this.setState({
             youthResults: [],
             formResults: results,
@@ -92,19 +95,32 @@ class SearchPage extends React.Component {
         return (
             <div className="column-container">
                 <div className="logo-container">
-                    <img className = "logo" src = {logo} alt= ""/>
+                    <img className="logo" src={logo} alt="" />
                 </div>
-                <div className = 'bar-container'>
+                <div className='bar-container'>
                     <h1 className="date">{this.state.date}</h1>
-                    <SearchBar updateYouthResults = {this.updateYouthResults} updateFormResults = {this.updateFormResults}
-                    updateEventResults = {this.updateEventResults}/>
+                    <SearchBar updateYouthResults={this.updateYouthResults} updateFormResults={this.updateFormResults}
+                        updateEventResults={this.updateEventResults} 
+                        />
                 </div>
                 <div className="search-summary">
-                <p>{this.state.searchSummary}</p>
+                    <p>{this.state.searchSummary}</p>
                 </div>
-                <div className = 'results-container'>
-                    <SearchResults youthResults = {this.state.youthResults}  formResults = {this.state.formResults} eventResults = {this.state.eventResults}/>
+                <div className='results-container'>
+                    <SearchResults youthResults={this.state.youthResults} formResults={this.state.formResults} eventResults={this.state.eventResults} />
                 </div>
+
+                <PDFDownloadLink document={<ReportGenerator youthResults={[1, 2, 3, 4]} formResults={this.state.formResults} eventResults={this.state.eventResults} />} fileName="FORM">
+                    {({ loading }) =>
+                        loading ? (
+                            <button>Loading Document ...</button>
+                        ) : (
+                            <button>Download</button>
+                        )
+                    }
+
+                </PDFDownloadLink>
+
             </div>
         )
     }

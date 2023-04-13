2b7b77d4885522f977bb42ba6197a44219383b0a
b-walton <benjamin.t.walton@vanderbilt.edu>
2023-04-12 21:43:14 -0500

    Fixed Youth Results Styling

--- src/components/YouthResult/YouthResult.js
+++ src/components/YouthResult/YouthResult.js
@@ -1,5 +1,6 @@
 import './YouthResult.scss';
 import React from "react";
+import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
 
@@ -10,26 +11,21 @@ class YouthResult extends React.Component {
   render() {
     return (
       <div className="youth-result">
-        <div className="youth-result-left">
           <div className='user-icon'>
-            <FontAwesomeIcon icon={faCircleUser}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faCircleUser} size ="3x"></FontAwesomeIcon>
           </div>
 
-          <div>
-            <p className="youth-name">{this.props.youth.firstName} {this.props.youth.lastName}</p>
+          <div className='text-holder'>
+            <p className="youth-name">{this.props.youth.firstName + " " + this.props.youth.lastName}</p>
             <p className='description'>Youth</p>
           </div>
-          <a href=''>
-            <button class="det-button">
+          <div className='button-holder'>
+          <Link to = {this.state.url} target = "_blank">
+            <button className="det-button">
               Details
             </button>
-          </a>
-
-        </div>
-
-        <div className='category-youth'>
-          Youth
-        </div>
+          </Link>
+          </div>
       </div>
     );
   }

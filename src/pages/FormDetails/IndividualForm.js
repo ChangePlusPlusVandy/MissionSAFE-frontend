a2c59227b86f59b9735fb13e3cd5f4cedfbfef3a
Salomon Dushimirimana <63796975+salomondush@users.noreply.github.com>
2023-04-16 22:44:02 -0500

    Update IndividualForm.js

--- src/pages/FormDetails/IndividualForm.js
+++ src/pages/FormDetails/IndividualForm.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
 import {getFormByID} from "../../util/ServerInterfaceForm";
-import "./FormDetails.scss";
+import "./IndividualForm.scss";
 import logo from "../../assets/mission-safe-logo.png"
 import dateFormat from "dateformat";
 
